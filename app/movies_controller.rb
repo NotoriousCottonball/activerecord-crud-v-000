4e671ebd50cf4f23a64795788064458d53da9f28
George  Pianka <gpianka1@gmail.com>
2018-10-15 12:18:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -16,7 +16,8 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-  Movie.create 
+  Movie.create do |m|
+    
   
 end
 
