8301f2058cfdb556f299f3b10ec12878c296dc51
George  Pianka <gpianka1@gmail.com>
2018-10-15 12:48:53 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -57,8 +57,8 @@ end
 
 def can_update_using_update_method
   Movie.create(title: "Wat?")
-  
-  
+  movie = Movie.find_by_title("Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
